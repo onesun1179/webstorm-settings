a015e19d5dd4a01254a0f6e2a0e5cf7b8368334c
dongwon <dongwon@unitsoft.kr>
2022-07-06 10:05:50 +0900

    WS-2022.1.3 <USER@DESKTOP-QDIEILG Create React Component.jsx, mdx.mdx, laf.xml, ide.general.xml, ide-features-trainer.xml, Windows.xml, project.default.xml, rainbow_brackets.xml, terminal.xml, Default.xml, React Hook.jsx, ignore.xml, find.xml, filetypes.xml, vcs.xml, JSON.json, markdown.md, editor-font.xml, colors.scheme.xml, KeyPromoterXSettings.xml, next page.tsx, Jsx.jsx, debugger.xml, updates.xml

---
+++ fileTemplates/React Component.jsx
@@ -0,0 +1,12 @@
+import { memo } from "react"
+import PropTypes from 'prop-types';
+
+const ${NAME} = memo(() => {
+    return null
+})
+
+export default ${NAME}
+
+${NAME}.propTypes = {}
+${NAME}.defaultProps = {}
+${NAME}.displayName = "${NAME}"
